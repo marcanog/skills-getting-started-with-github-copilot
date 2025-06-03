bb3897c209701d4b8035336232b943b1bfc5b9f5
Georgelys Marcano <57365496+marcanog@users.noreply.github.com>
2025-06-03 20:55:31 +0000

    Add comment

--- src/app.py
+++ src/app.py
@@ -51,11 +51,11 @@ def root():
 def get_activities():
     return activities
 
-
+# 
 @app.post("/activities/{activity_name}/signup")
 def signup_for_activity(activity_name: str, email: str):
     """Sign up a student for an activity"""
-    # Validate activity exists
+  
     if activity_name not in activities:
         raise HTTPException(status_code=404, detail="Activity not found")
 
@@ -64,4 +64,3 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Add student
     activity["participants"].append(email)
-    return {"message": f"Signed up {email} for {activity_name}"}
